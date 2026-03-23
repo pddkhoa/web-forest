a8fdadd53466b1fdc4b500d17394015d9525067f
unknown <khoapdd@hpt.vn>
2026-03-23 13:47:06 +0700

    init code

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
+export default {
+  darkMode: ["class"],
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["'Instrument Serif'", "serif"],
+        body: ["'Barlow'", "sans-serif"],
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+}
